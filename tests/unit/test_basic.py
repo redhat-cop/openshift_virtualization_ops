e63a4a7c9bca8ba45e4dc4b465f9bf7b88ea440a
shellclear <12013211+shellclear@users.noreply.github.com>
2025-12-12 09:55:47 +0100

    lint

---
+++ tests/unit/test_basic.py
@@ -0,0 +1,10 @@
+"""Unit tests for infra.openshift_virtualization_ops."""
+
+
+def test_basic() -> None:
+    """Dummy unit test that always passes.
+
+    Raises:
+        AssertionError: If the assertion fails.
+    """
+    assert bool(1) is True
